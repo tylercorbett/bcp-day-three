93a8a17f311ca5fd9cea5259b163c25dd397fb75
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-03 20:43:06 -0700

    Added two new prompts

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-for(var i = 0; i <= 100; i++) {
+var countTo = prompt('How high should we count?');
+
+var sure = confirm('Are you sure you want to count to ' + countTo + '?');
+
+for(var i = 0; i <= countTo; i++) {
     if (i % 2 === 0) {
         console.log('The current number is', i);
     }
